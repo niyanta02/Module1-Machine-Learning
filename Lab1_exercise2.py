d1188d4b93e9b49ca26bdba1ea6e532a7b47ed21
niyanta02 <niyanta.official@gmail.com>
2022-06-18 13:33:20 -0400

    print theta_best

--- Lab1_exercise2.py
+++ Lab1_exercise2.py
@@ -5,4 +5,20 @@ Created on Sat Jun 18 13:14:00 2022
 @author: niyan
 """
 
-"""linear regression"""
+"""linear regression
+Close form solution"""
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+x = 2 * np.random.rand(100,1)
+y = 4 + 3 * x + np.random.rand(100,1)
+
+
+x_b = np.c_[np.ones((100,1)),x]
+
+theta_best = np.linalg.inv(x_b.T.dot(x_b)).dot(x_b.T).dot(y)
+print(theta_best)
+
+
