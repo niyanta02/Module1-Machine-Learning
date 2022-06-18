f89c317e2a9454ab72c5dfb1068468a6215d03ef
niyanta02 <niyanta.official@gmail.com>
2022-06-18 14:09:11 -0400

    interaction_only and include_bias

--- Lab1_exercise3.py
+++ Lab1_exercise3.py
@@ -13,9 +13,9 @@ from sklearn.preprocessing import PolynomialFeatures
 x = np.arange(6).reshape(3,2)
 print(x)
 
-poly = PolynomialFeatures(degree=2)
+poly = PolynomialFeatures(degree=2 , interaction_only = True, include_bias = False)
 transformed = poly.fit_transform(x)
 print(transformed)
-
+print(transformed.shape[1])
 #feature name
 print(poly.get_feature_names(input_features= None))
