8a21e8dfe73c180a5095253c619ee6ee714ebbd6
niyanta02 <niyanta.official@gmail.com>
2022-06-18 14:00:43 -0400

    array of size 3,2

---
+++ Lab1_exercise3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 18 13:55:34 2022
+
+@author: niyan
+"""
+
+"""Polynomial regression"""
+
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+
+x = np.arange(6).reshape(3,2)
+print(x)
