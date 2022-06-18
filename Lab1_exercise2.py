77ef3f7627759c9510c3dfa0898157913bdf64e3
niyanta02 <niyanta.official@gmail.com>
2022-06-18 13:54:37 -0400

    Stochastic Gradient Descent

--- Lab1_exercise2.py
+++ Lab1_exercise2.py
@@ -55,14 +55,14 @@ plt.plot(x, y, "b.")
 plt.axis([0, 2, 0, 15])
 plt.show()
 
-#### ## doing Stochastic Gradient Descent USING SKLEARN
+## Stochastic Gradient Descent USING SKLEARN
 n_epochs = 50
 t0, t1 = 5, 50  # learning schedule hyperparameters
 m = 100
-X = 2 * np.random.rand(100, 1)
-y = 4 + 3 * x + np.random.randn(100, 1)
+#X = 2 * np.random.rand(100, 1)
+#y = 4 + 3 * x + np.random.randn(100, 1)
 
-X_b = np.c_[np.ones((100, 1)), X]
+x_b = np.c_[np.ones((100, 1)), x]
 
 def learning_schedule(t):
     return t0 / (t + t1)
@@ -72,7 +72,7 @@ theta = np.random.randn(2,1)  # random initialization
 for epoch in range(n_epochs):
     for i in range(m):
         random_index = np.random.randint(m)
-        xi = X_b[random_index:random_index+1]
+        xi = x_b[random_index:random_index+1]
         yi = y[random_index:random_index+1]
         gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
         eta = learning_schedule(epoch * m + i)
