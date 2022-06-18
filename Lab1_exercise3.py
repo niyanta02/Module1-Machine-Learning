6daadd701faabbf50d87bb3b91828486fcb0cbd6
niyanta02 <niyanta.official@gmail.com>
2022-06-18 14:04:39 -0400

    created a poly variable of degree 2

--- Lab1_exercise3.py
+++ Lab1_exercise3.py
@@ -12,3 +12,7 @@ from sklearn.preprocessing import PolynomialFeatures
 
 x = np.arange(6).reshape(3,2)
 print(x)
+
+poly = PolynomialFeatures(degree=2)
+transformed = poly.fit_transform(x)
+print(transformed)
